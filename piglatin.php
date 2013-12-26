4c225636a5bdba626a77f1e9f500eb91eac8febf
Ulrich Pogson <grapplerulrich@gmail.com>
2013-12-26 01:08:31 +0100

    Clean up + add button to admin bar

--- piglatin.php
+++ piglatin.php
@@ -1,8 +1,8 @@
 <?php
 /*
- * Plugin Name:	Pig Latin
- * Plugin URI: http://wordpress.org/extend/plugins/piglatin/
-* Description: Overrides the current language and translates all messages into pig latin. This way you can easily spot, which messages were left untranslatable, while the interface is still usable.
+ * Plugin Name: Pig Latin
+ * Plugin URI: http://wordpress.org/plugins/piglatin/
+ * Description: Overrides the current language and translates all messages into pig latin. This way you can easily spot, which messages were left untranslatable, while the interface is still usable.
  * Version: 0.1
  * Author: Nikolay Bachiyski
  * Author URI: http://nikolay.bg/
@@ -10,75 +10,125 @@
 
 class PigLatin {
 
-	function word2pig($match) {
-	    $text = $match[0];
-	    $hyphen = '';
-	    $consonants = "bBcCdDfFgGhHjJkKlLmMnNpPqQrRsStTvVwWxXyYzZ";
-	    $vowels = "aAeEiIoOuU";
+	public function word2pig( $match ) {
+		$text       = $match[0];
+		$hyphen     = '';
+		$consonants = "bBcCdDfFgGhHjJkKlLmMnNpPqQrRsStTvVwWxXyYzZ";
+		$vowels     = "aAeEiIoOuU";
 
 		$i = 0;
-		if (false !== strpos($consonants, $text[0])) {
+		if ( false !== strpos( $consonants, $text[0] ) ) {
 			$cons = $text[0];
 			$i = 1;
-			while ($i < strlen($text) && false !== strpos($consonants, $text[$i])) {
+			while ( $i < strlen($text) && false !== strpos( $consonants, $text[$i] ) ) {
 				$cons .= $text[$i];
 				++$i;
 			}
 			return substr($text, $i).$hyphen.$cons.'ay';
-		}
-		else if (false !== strpos($vowels, $text[0])) {
+		} else if ( false !== strpos( $vowels, $text[0] ) ) {
 			return $text.'ay';
-		}
-		else {
+		} else {
 			return $text;
 		}
 	}
 
-
-	function translation2pig($string) {
-	    if (strlen($string) < 3) {
-	        return $string;
-	    }
+	public function translation2pig( $string ) {
+		if ( strlen( $string ) < 3 ) {
+			return $string;
+		}
 		/*
 			do not translate tag names and attributes,
 			entities, and %xxx encoded strings
 		*/
-	    $delimiters = array(
-	        '<.*?>',
-	        '\&#\d+;',
-	        '\&[a-z]+;',
-	        '%\d+\$[sd]',
-	        '%[sd]',
-	        '\s+',
-	    );
-	    $parts = preg_split('/('.implode('|', $delimiters).')/i', $string, -1, PREG_SPLIT_DELIM_CAPTURE | PREG_SPLIT_NO_EMPTY);
-	    $cnt = count($parts);
-	    for ($i = 0; $i < $cnt; ++$i) {
-	        $isdelim = false;
-	        foreach ($delimiters as $delim) {
-	            if (preg_match("/^$delim$/", $parts[$i])) {
-	                $isdelim = true;
-	                break;
-	            }
-	        }
-	        if ($isdelim) {
-	            continue;
-	        }
-	        $parts[$i] = preg_replace_callback('/[a-z]+/i', array('PigLatin', 'word2pig'),$parts[$i]);
-	    }
-	    return implode('', $parts);
+		$delimiters = array(
+			'<.*?>',
+			'\&#\d+;',
+			'\&[a-z]+;',
+			'%\d+\$[sd]',
+			'%[sd]',
+			'\s+',
+		);
+		$parts = preg_split( '/('.implode('|', $delimiters).')/i', $string, -1, PREG_SPLIT_DELIM_CAPTURE | PREG_SPLIT_NO_EMPTY );
+		$cnt = count( $parts );
+		for ( $i = 0; $i < $cnt; ++$i ) {
+			$isdelim = false;
+			foreach ( $delimiters as $delim ) {
+				if ( preg_match( "/^$delim$/", $parts[$i] ) ) {
+					$isdelim = true;
+					break;
+				}
+			}
+			if ($isdelim) {
+				continue;
+			}
+			$parts[$i] = preg_replace_callback( '/[a-z]+/i', array( 'PigLatin', 'word2pig' ), $parts[$i] );
+		}
+		return implode( '', $parts );
 	}
 
-	function gettext($translated, $original) {
-	    return PigLatin::translation2pig($original);
+	public function gettext( $translated, $original ) {
+		return PigLatin::translation2pig( $original );
 	}
 
-	function ngettext($translated, $single, $plural, $number) {
+	public function ngettext( $translated, $single, $plural, $number ) {
 		return PigLatin::translation2pig($number == 1? $single : $plural);
 	}
-};
 
-add_filter('gettext', array('PigLatin', 'gettext'), 10, 2);
-add_filter('gettext_with_context', array('PigLatin', 'gettext'), 10, 2);
-add_filter('ngettext', array('PigLatin', 'ngettext'), 10, 4);
-add_filter('ngettext_with_context', array('PigLatin', 'ngettext'), 10, 4);
+	/**
+	 * Adds button to admin bar.
+	 *
+	 * @global object $wp_admin_bar Most likely instance of WP_Admin_Bar but this is filterable.
+	 *
+	 * @return null Retuns early if not site admin, or admin bar should not be showing.
+	 *
+	 * @since 0.2
+	 */
+	public function admin_bar_piglatin_switcher() {
+		global $wp_admin_bar;
+		$_user_id = get_current_user_id();
+
+		if ( ! is_super_admin() || ! is_admin_bar_showing() )
+			return;
+
+		// Get opposite direction for button text
+		$piglatin = get_user_meta( $_user_id, 'piglatinadminbar', true );
+		$piglatin = 'true' == $piglatin ? 'false' : 'true';
+		$title = 'true' == $piglatin ? 'Activate Pig Latin' : 'Deactivate Pig Latin';
+
+		$wp_admin_bar->add_menu(
+			array(
+				'id'    => 'PigLatin',
+		 		'title' => $title,
+		 		'href'  => add_query_arg( array( 'piglatin' => $piglatin ) )
+			)
+		);
+	}
+
+	/**
+	 * Save the currently chosen state on a per-user basis.
+	 *
+	 * @since 0.2
+	 */
+	public function set_piglatin() {
+
+		$_user_id = get_current_user_id();
+		$piglatin = isset( $_GET['piglatin'] ) ? $_GET['piglatin'] : get_user_meta( $_user_id, 'piglatinadminbar', true );
+
+		if ( isset( $_GET['piglatin'] ) ) {
+			$piglatin = $_GET['piglatin'] == 'true' ? 'true' : 'false';
+			update_user_meta( $_user_id, 'piglatinadminbar', $piglatin );
+		}
+
+		if ( 'true' == $piglatin ) {
+			add_filter( 'gettext', array( 'PigLatin', 'gettext' ), 10, 2 );
+			add_filter( 'gettext_with_context', array( 'PigLatin', 'gettext' ), 10, 2 );
+			add_filter( 'ngettext', array( 'PigLatin', 'ngettext' ), 10, 4 );
+			add_filter( 'ngettext_with_context', array( 'PigLatin', 'ngettext' ), 10, 4 );
+		}
+
+	}
+
+}
+
+add_action( 'init', array( 'PigLatin', 'set_piglatin' ) );
+add_action( 'admin_bar_menu', array( 'PigLatin', 'admin_bar_piglatin_switcher' ), 999 );
--- readme.txt
+++ readme.txt
@@ -2,7 +2,7 @@
 Contributors: nbachiyski
 Tags: piglatin, i18n, translation, translations, plugin, theme, plugins, themes
 Requires at least: 2.7
-Tested up to: 2.9
+Tested up to: 3.8
 Stable tag: 0.1
 
 Overrides the current language and translates all messages into Pig Latin.
@@ -23,6 +23,25 @@ Text in Pig Latin is both noticeably different than English and readable. This w
 
 This section describes how to install the plugin and get it working.
 
-1. Unzip `piglatin-x.x.zip`
-1. Upload the `piglatin` directory (not its contents, the whole directory) to `/wp-content/plugins/`
-1. Activate the plugin through the `Plugins` menu in WordPress
+= Installing from the WordPress dashboard =
+
+1. Navigate to the 'Add New' plugins dashboard
+2. Search for 'Pig Latin'
+3. Click 'Install Now'
+4. Activate the plugin on the WordPress Plugin dashboard
+
+= Uploading in the WordPress dashboard =
+
+1. Download `piglatin.zip`
+2. Navigate to the 'Add New' plugins dashboard
+3. Navigate to the 'Upload' area
+4. Select `piglatin.zip` from your computer
+5. Upload
+6. Activate the plugin on the WordPress Plugin dashboard
+
+= Using FTP =
+
+1. Download `piglatin.zip`
+2. Extract the `piglatin` directory to your computer
+3. Upload the `piglatin` directory to your `wp-content/plugins` directory
+4. Activate the plugin on the WordPress Plugins dashboard
